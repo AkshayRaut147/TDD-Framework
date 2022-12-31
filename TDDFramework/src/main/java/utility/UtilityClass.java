fa757902efb67ee09acda0609cf4d83f088e9542
AkshayRaut147 <akshayraut1470@gmail.com>
2022-12-31 18:48:56 +0530

    First Commit

---
+++ TDDFramework/src/main/java/utility/UtilityClass.java
@@ -0,0 +1,46 @@
+package utility;
+
+import testBase.TestBase;
+
+public class UtilityClass extends TestBase{
+
+	public void screenshot()
+	{
+		
+	}
+	
+	public void expliWaitClickable()
+	{
+		
+	}
+	
+	public void expliWaitAlert()
+	{
+		
+	}
+	
+	public void exelRead()
+	{
+		
+	}
+	
+	public void exelWrite()
+	{
+		
+	}
+	
+	public void exelRowCount()
+	{
+		
+	}
+	
+	public void exelColumnCount()
+	{
+		
+	}
+	
+	public void scroll()
+	{
+		
+	}
+}
