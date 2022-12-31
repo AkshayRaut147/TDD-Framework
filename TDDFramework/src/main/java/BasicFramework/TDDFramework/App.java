96acbf62a65597fca49a3b0765699a9e3f40458d
AkshayRaut147 <akshayraut1470@gmail.com>
2022-12-31 17:22:26 +0530

    First Commit

---
+++ TDDFramework/src/main/java/BasicFramework/TDDFramework/App.java
@@ -0,0 +1,13 @@
+package BasicFramework.TDDFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
